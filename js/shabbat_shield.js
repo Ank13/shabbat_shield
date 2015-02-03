75c4a370ff03d4ca8ac4e3071bee5baacf26622f
ank13 <avi.kaufman@gmail.com>
2015-02-02 19:21:01 -0500

    more text

--- js/shabbat_shield.js
+++ js/shabbat_shield.js
@@ -2,7 +2,6 @@ var date = new Date();
 var day = date.getDay(); 
 var hour = date.getHours();
 var minutes = date.getMinutes();
-
 // run only once the document has loaded
 window.onload = init;
 
@@ -10,10 +9,12 @@ function init(){
   // the div that will hide the page
   var shield = document.getElementById('shield');
 
-  if(day == 5 && hour > 17 || day == 6 && hour < 18){
+  if(day == 5 && hour >= 13 || day == 6 && hour <= 18){
     shield.style.display = 'block'
   }
   else {
     shield.style.display = 'none'
   }
 }
+
+// overwrite body
